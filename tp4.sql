ee1fb4e4f90555eb4aa1933a5933b60f1e8afc44
joaquinbian <joacobian21@gmail.com>
2025-09-13 17:29:28 -0300

    tp4

--- tp4.sql
+++ tp4.sql
@@ -30,3 +30,32 @@ OR  TiposArchivos.TipoArchivo LIKE '%GIF%' ;
 
 
 -- Listar los nombres de archivos, su extensión, el tamaño en megabytes y la fecha de creación de aquellos archivos que le pertenezcan al usuario dueño con nombre 'Michael' y apellido 'Williams'.
+
+SELECT Archivos.Nombre, Extension, Tamanio AS 'Tamanio en Bytes', Tamanio / 1048576 AS 'Tamanio en MB', FechaCreacion FROM Archivos
+INNER JOIN Usuarios ON Archivos.IDUsuarioDuenio = Usuarios.IDUsuario
+WHERE Usuarios.Nombre LIKE 'Michael' AND Usuarios.Apellido LIKE 'Williams';
+
+
+-- Listar los datos completos del archivo más pesado del usuario dueño con nombre 'Michael' y apellido 'Williams'. 
+-- Si hay varios archivos que cumplen esa condición, listarlos a todos.
+
+SELECT * FROM Archivos
+INNER JOIN Usuarios ON Archivos.IDUsuarioDuenio = Usuarios.IDUsuario
+WHERE Usuarios.Nombre LIKE 'Michael' AND Usuarios.Apellido LIKE 'Williams'
+ AND Archivos.Tamanio = (
+    SELECT MAX(Tamanio) FROM Archivos INNER JOIN Usuarios ON Archivos.IDUsuarioDuenio = Usuarios.IDUsuario
+    WHERE Usuarios.Nombre LIKE 'Michael' AND Usuarios.Apellido LIKE 'Williams'
+ )
+;
+
+-- Listar nombres de archivos, extensión, tamaño en bytes, fecha de creación y de modificación, apellido y nombre del usuario dueño de aquellos archivos cuya descripción contengan el término 'empresa' o 'presupuesto'
+
+SELECT Archivos.Nombre, Extension, Tamanio, FechaCreacion, FechaUltimaModificacion, Usuarios.Apellido, Usuarios.Nombre FROM Archivos
+INNER JOIN Usuarios ON Archivos.IDUsuarioDuenio = Usuarios.IDUsuario
+WHERE Descripcion LIKE '%empresa%' OR Descripcion LIKE '%presupuesto%';
+
+-- Listar las extensiones sin repetir de los archivos cuyos usuarios dueños tengan tipo de usuario 'Suscripción Plus', 'Suscripción Premium' o 'Suscripción Empresarial'
+SELECT Extension, TipoUsuario FROM Archivos 
+INNER JOIN Usuarios ON IDUsuarioDuenio = IDUsuario
+INNER JOIN TiposUsuario ON Usuarios.IDTipoUsuario = TiposUsuario.IDTipoUsuario 
+WHERE TipoUsuario LIKE 'Suscripcion Plus' OR TipoUsuario LIKE 'Suscripcion Empresarial' OR TipoUsuario LIKE 'Suscripcion Premium';
