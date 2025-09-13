c751b8dd24f545e9a830f872e0d708a8d1cf3b30
joaquinbian <joacobian21@gmail.com>
2025-09-13 16:31:36 -0300

    tp4

--- tp4.sql
+++ tp4.sql
@@ -9,3 +9,24 @@ SELECT Nombre, Apellido, TiposUsuario.TipoUsuario FROM Usuarios INNER JOIN Tipos
 SELECT Nombre, Apellido, TiposUsuario.TipoUsuario FROM Usuarios 
 INNER JOIN TiposUsuario ON Usuarios.IDTipoUsuario = TiposUsuario.IDTipoUsuario 
 WHERE TiposUsuario.TipoUsuario LIKE 'Suscripcion Free' OR TiposUsuario.TipoUsuario LIKE 'Suscripcion Basica';
+
+
+-- Listar los nombres de archivos, extensión, tamaño expresado en Megabytes y el nombre del tipo de archivo.
+--NOTA: En la tabla Archivos el tamaño está expresado en Bytes.
+
+SELECT Nombre, Extension, Tamanio AS 'Tamanio en Bytes', Tamanio / 1048576 AS 'Tamanio en MB' FROM Archivos
+INNER JOIN TiposArchivos ON Archivos.IDTipoArchivo = TiposArchivos.IDTipoArchivo;
+
+
+-- Listar los nombres de archivos junto a la extensión con el siguiente formato 'NombreArchivo.extension'. Por ejemplo, 'Actividad.pdf'.
+--Sólo listar aquellos cuyo tipo de archivo contenga los términos 'ZIP', 'Word', 'Excel', 'Javascript' o 'GIF'
+SELECT Nombre + '.' + Extension AS 'Archivo' FROM Archivos
+INNER JOIN TiposArchivos ON Archivos.IDTipoArchivo = TiposArchivos.IDTipoArchivo
+WHERE TiposArchivos.TipoArchivo LIKE '%ZIP%' 
+OR  TiposArchivos.TipoArchivo LIKE '%JavaScript%' 
+OR  TiposArchivos.TipoArchivo LIKE '%Word%' 
+OR  TiposArchivos.TipoArchivo LIKE '%Excel%' 
+OR  TiposArchivos.TipoArchivo LIKE '%GIF%' ;
+
+
+-- Listar los nombres de archivos, su extensión, el tamaño en megabytes y la fecha de creación de aquellos archivos que le pertenezcan al usuario dueño con nombre 'Michael' y apellido 'Williams'.
