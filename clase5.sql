9508117da473fc1fecf615e1832d7c6baa37fdc2
joaquinbian <joacobian21@gmail.com>
2025-09-30 20:12:51 -0300

    clase7

--- clase5.sql
+++ clase5.sql
@@ -4,3 +4,25 @@
     -- max
     -- min
     -- sum
+
+
+-- count:
+    -- select count(*) from libros
+    -- select count(idCategoria) from libros -- 5, 10, 5, NULL (devuelve 3)
+    -- select count(distinct idCategoria) from libros -- 5, 10, 5, NULL (devuelve 2)
+
+
+
+-- avg
+-- max
+-- min
+-- sum
+
+
+
+---------------------------
+-- group by
+    -- resume agrupando por una o mas condiciones
+-- having
+    -- aplica un filtrado al resultado de una funcion de resumen
+
