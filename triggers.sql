d1c11f923f4945c2d4c8c629e00bcaaa19894526
joaquinbian <joacobian21@gmail.com>
2025-10-04 12:56:32 -0300

    triggers

--- triggers.sql
+++ triggers.sql
@@ -8,5 +8,11 @@ CREATE TRIGGER TR_DELETE_CUENTA_BANCARIA ON Cuentas
 INSTEAD OF DELETE
 AS
 BEGIN
-    SELECT * FROM Cuentas;
+    SELECT * FROM deleted; --la tabla temporal deleted tiene los datos que se quisieron eliminar
 END
+
+SELECT * FROM Cuentas;
+
+DROP TRIGGER TR_DELETE_CUENTA_BANCARIA;
+
+
