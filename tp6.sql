a0cc820407c88be3733578b6cdbd33a5e070838b
joaquinbian <joacobian21@gmail.com>
2025-10-11 10:55:30 -0300

    subconsultas

--- tp6.sql
+++ tp6.sql
@@ -63,4 +63,71 @@ SELECT Nombre, Extension from Archivos WHERE Tamanio > (
 
 
 -- Por cada tipo de archivo indicar el tipo y la cantidad de archivos eliminados y la cantidad de archivos no eliminados.
+SELECT TA.TipoArchivo, 
+(
+    SELECT COUNT(*) FROM Archivos A WHERE Eliminado = 1 AND A.IDTipoArchivo = TA.IDTipoArchivo
+) AS 'Archivos Eliminados',
+(
+    SELECT COUNT(*) FROM Archivos A WHERE Eliminado = 0 AND A.IDTipoArchivo = TA.IDTipoArchivo
+) AS 'Archivos NO Eliminados'
+FROM TiposArchivos TA
 
+
+
+--Por cada usuario indicar el IDUsuario, el apellido, el nombre 
+--la cantidad de archivos pequeños (menos de 20MB) y la cantidad de archivos grandes (20MBs o más)
+DECLARE @BYTES_TO_MB int
+SET @BYTES_TO_MB = 1048576
+
+SELECT U.IDUsuario, U.Apellido, U.Nombre,
+(
+    SELECT COUNT(*) FROM Archivos A WHERE U.IDUsuario = A.IDUsuarioDuenio AND A.Tamanio * 1.0 / @BYTES_TO_MB < 20 
+) AS 'Archivos Pequenios',
+(
+    SELECT COUNT(*) FROM Archivos A WHERE U.IDUsuario = A.IDUsuarioDuenio AND A.Tamanio * 1.0 / @BYTES_TO_MB >= 20 
+
+) AS 'Archivos Grandes'
+FROM Usuarios U
+
+-- Por cada usuario indicar el IDUsuario, el apellido, el nombre 
+-- y la cantidad de archivos creados en el año 2022, la cantidad en el año 2023 y la cantidad creados en el 2024.
+
+SELECT 
+U.IDUsuario, U.Apellido, U.Nombre,
+(
+    SELECT COUNT(*) FROM Archivos A WHERE A.IDUsuarioDuenio = U.IDUsuario AND YEAR(A.FechaCreacion) = 2022
+) AS 'Archivos 2022',
+(
+    SELECT COUNT(*) FROM Archivos A WHERE A.IDUsuarioDuenio = U.IDUsuario AND YEAR(A.FechaCreacion) = 2023
+) AS 'Archivos 2023',
+(
+    SELECT COUNT(*) FROM Archivos A WHERE A.IDUsuarioDuenio = U.IDUsuario AND YEAR(A.FechaCreacion) = 2024
+) AS 'Archivos 2024'
+ FROM Usuarios U
+
+
+ -- Los archivos que fueron compartidos con permiso de 'Comentario' pero no con permiso de 'Lectura'
+
+SELECT * FROM (
+SELECT a.Nombre, 
+(
+    SELECT COUNT(*) FROM ArchivosCompartidos AC 
+    INNER JOIN Permisos P ON P.IDPermiso = AC.IDPermiso
+    WHERE Nombre = 'Lectura' AND AC.IDArchivo = A.IDArchivo
+)AS 'Archivos Lectura',
+(
+    SELECT COUNT(*) FROM ArchivosCompartidos AC 
+    INNER JOIN Permisos P ON P.IDPermiso = AC.IDPermiso
+    WHERE Nombre = 'Comentario' AND AC.IDArchivo = A.IDArchivo
+)AS 'Archivos Comentario'
+
+ FROM Archivos A
+) AS Auxiliar WHERE [Archivos Comentario] >= 1 AND [Archivos Lectura] = 0
+
+
+-- Los tipos de archivos que registran más archivos eliminados que no eliminados.
+SELECT * FROM (SELECT 
+TA.TipoArchivo,
+(SELECT COUNT(*) FROM Archivos A WHERE Eliminado = 1 AND A.IDTipoArchivo = TA.IDTipoArchivo) as ArchivosEliminados,  
+(SELECT COUNT(*) FROM Archivos A WHERE Eliminado = 0 AND A.IDTipoArchivo = TA.IDTipoArchivo) as ArchivosNOEliminados 
+ FROM TiposArchivos TA) AS AUXILIAR WHERE ArchivosEliminados > ArchivosNOEliminados
